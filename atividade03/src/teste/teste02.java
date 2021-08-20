fa7c6675f7980d3aad52a124cb1013edcf09b479
thays100caet <thayscaetano100@semprecub.com>
2021-08-19 21:26:05 -0300

    aula do dia 10/08 adt 03

---
+++ atividade03/src/teste/teste02.java
@@ -0,0 +1,34 @@
+package teste;
+
+
+	import java.io.IOException;
+	import java.io.PrintWriter;
+	import java.util.Enumeration;
+
+	import javax.servlet.ServletException;
+	import javax.servlet.annotation.WebServlet;
+	import javax.servlet.http.HttpServlet;
+	import javax.servlet.http.HttpServletRequest;
+	import javax.servlet.http.HttpServletResponse;
+
+	@WebServlet("/ServletListaNomeParametros")
+	public class teste02 extends HttpServlet {
+		private static final long serialVersionUID = 1L;
+
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	          PrintWriter resposta = response.getWriter();
+	          resposta.write("<html><body>");
+	          resposta.write("Olá,turma  <BR>");
+	          Enumeration<String> nomesParametros = request.getParameterNames();
+	          while (nomesParametros.hasMoreElements()) {
+	               resposta.write(nomesParametros.nextElement().toString());
+	               resposta.write(", ");
+	          }
+	          resposta.write("</html></body>");
+	       }
+
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			doGet(request, response);
+		}
+	}
+
